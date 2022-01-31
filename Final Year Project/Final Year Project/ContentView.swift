7f6d44f2b3de09e72442f79bac6b34b561d0dc88
simonasramonas <sramona@yahoo.com>
2022-01-31 14:41:48 +0000

    Xcode project added.

---
+++ Final Year Project/Final Year Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final Year Project
+//
+//  Created by Simonas Ramonas on 31/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Year Project/Final Year Project/Final_Year_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Year_ProjectApp.swift
+//  Final Year Project
+//
+//  Created by Simonas Ramonas on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Final_Year_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
