7abfe7b12e08216e8c262110402a1727cb50625d
SimonRamone <sramonas@yahoo.com>
2022-02-01 12:53:12 +0000

    Updated Home View.

--- Final Year Project/Final Year Project/Views/HomeView.swift
+++ Final Year Project/Final Year Project/Views/HomeView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        Text("Home View")
-    }
+    @State var carbonEmissionsValue: Float = 13.2
+        
+        var body: some View {
+            ZStack {
+                VStack {
+                    ZStack {
+                        Circle()
+                            .stroke(lineWidth: 20.0)
+                            .opacity(0.6)
+                            .foregroundColor(Color.red)
+
+                        VStack {
+                            Text(String(format: "%.1f CO\u{2082}e", carbonEmissionsValue))
+                                .font(.largeTitle)
+                                .bold()
+                            Text("Tonnes per annum")
+                        }
+                    }
+                }
+            }
+            .padding(60)
+        }
 }
 
 struct HomeView_Previews: PreviewProvider {
